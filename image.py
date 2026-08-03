6a4f7898270960f4f920acb3a54f2751c47a6928
ilyas-dar <aleeilyas14@gmail.com>
2026-08-03 08:59:07 +0000

    comparision b/w gbr BINARY thresholding and grey scale thresholding

--- image.py
+++ image.py
@@ -6,11 +6,19 @@ import cv2
 img = cv2.imread("resized_khm.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+ret, thresh=cv2.threshold(gray, 127,225,cv2.THRESH_BINARY)
+bret, thresht=cv2.threshold(img, 127,225,cv2.THRESH_BINARY)
+print(ret)
+print(bret)
+cv2.imshow("Threshold", thresh)
+cv2.imshow("Thresholdd", thresht)
 # blr = cv2.GaussianBlur(img, (5,5),0)
 
-cv2.imshow("Original", img)
-edge=cv2.Canny(img, 100, 200)
-cv2.imshow("Edges", edge )
+# cv2.imshow("Original", img)
+# edge=cv2.Canny(img, 100, 200)
+# cv2.imshow("Edges", edge )
 
 
 # cv2.imshow("Gray", gray)
